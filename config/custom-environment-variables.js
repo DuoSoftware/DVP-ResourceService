35da8aad6ab21d754827155a919223c35e41bcac
achinthau <achintha@duosoftware.com>
2016-12-01 17:30:12 +0530

    Update custom-environment-variables.js

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -20,8 +20,8 @@ module.exports = {
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "password":"SYS_REDIS_PASSWORD",
-        "redisdb":"SYS_REDIS_DB",
-        "ardsData":"SYS_ARDS_DB"
+        "redisdb":"SYS_REDIS_DB_DASHBOARD",
+        "ardsData":"SYS_REDIS_DB_ARDS"
     }
     ,
 
