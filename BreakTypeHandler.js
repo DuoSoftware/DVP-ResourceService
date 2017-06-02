c057be6574b0598a4c733b57b0181b1629d80252
heshanindika <k.heshan.indika@gmail.com>
2017-06-02 16:37:06 +0530

    add reference 'redis'

--- BreakTypeHandler.js
+++ BreakTypeHandler.js
@@ -6,6 +6,7 @@ var messageFormatter = require('dvp-common/CommonMessageGenerator/ClientMessageJ
 var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
 var DbConn = require('dvp-dbmodels');
 var util = require('util');
+var redis = require('redis');
 
 var redisArdsClient = redis.createClient(config.ArdsRedis.port, config.ArdsRedis.ip);
 redisArdsClient.auth(config.ArdsRedis.password, function (err) {
