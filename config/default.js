4caca0508a14cd823a0904197c13ec3d83f946b2
achinthau <achintha@duosoftware.com>
2017-08-01 11:38:48 +0530

    Update default.js
    add sentinel variables

--- config/default.js
+++ config/default.js
@@ -12,7 +12,12 @@ module.exports = {
     "ip": "45.55.142.207",
     "port": 6389,
     "password":"DuoS123",
-      "redisdb":8
+      "redisdb":8,
+    "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
   },
 
     "ArdsRedis":
@@ -28,7 +33,12 @@ module.exports = {
         "ip": "45.55.142.207",
         "port": 6389,
         "user": "DuoS123",
-        "password": "DuoS123"
+        "password": "DuoS123",
+      "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
 
     },
 
